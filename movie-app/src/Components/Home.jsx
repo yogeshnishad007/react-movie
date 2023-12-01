9ca054eaf7464bf41d49df1a53e6b9ed399f9068
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-12-01 17:34:34 +0530

    final1

--- movie-app/src/Components/Home.jsx
+++ movie-app/src/Components/Home.jsx
@@ -7,54 +7,54 @@ const Home = () => {
   const [data, setData] = useState([]);
   const [searchMovie, setSearchMovie] = useState("");
   const [sortBy, setSortBy] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // Fetching API Data
   const fetchData = async (searchTerm) => {
-    const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=2f0fc567&s=${searchTerm}`);
-    const result = await response.json();
-    setData(result.Search);
-    console.log("data", result.Search);
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=2f0fc567&s=${searchTerm}`);
+      const result = await response.json();
+
+      if (result.Response === "True") {
+        setData(result.Search);
+      } else {
+        setData([]);
+      }
+    } catch (error) {
+      setError("An error occurred while fetching data.");
+    } finally {
+      setLoading(false);
+    }
   };
 
-
   // Search Movies by Name Logics
-
-  
   const handleSearch = () => {
     fetchData(searchMovie);
     setSearchMovie("");
   };
 
-
   // Add To Favorites Logics
-
   const handleAdd = (el) => {
     const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-      console.log(favorites)
 
     const checkMovie = favorites.find((fav) => fav.imdbID === el.imdbID);
-    console.log("check", checkMovie);
-  
-    if (checkMovie!==undefined) {
+
+    if (checkMovie !== undefined) {
       alert('Movie is already in favorites!');
-      
     } else {
-  
       favorites.push(el);
-      localStorage.setItem('favorites', JSON.stringify(favorites))
-
+      localStorage.setItem('favorites', JSON.stringify(favorites));
       alert('Movie added to favorites!');
-
     }
   };
-  
-
 
   useEffect(() => {
-    
     fetchData("hulk");
-  }, []); 
-
+  }, []);
 
   // Sorting Logic Here
   const handleSortBy = (criteria) => {
@@ -64,21 +64,16 @@ const Home = () => {
   const sortData = () => {
     let sortedData = [...data];
 
-
     if (sortBy === "year") {
       sortedData = sortedData.sort((a, b) => parseInt(a.Year, 10) - parseInt(b.Year, 10));
-    } 
+    }
 
     setData(sortedData);
   };
 
-
-
   useEffect(() => {
     sortData();
   }, [sortBy]);
-
-
 
   return (
     <>
@@ -89,48 +84,43 @@ const Home = () => {
           value={searchMovie}
           onChange={(e) => setSearchMovie(e.target.value)}
         />
-        <button onClick={handleSearch}>Search</button>
+        <button onClick={handleSearch} disabled={loading}>
+          Search
+        </button>
       </div>
 
-         
-               
-         <div className="sort-bar">
-            <label>Sort by:</label>
-            <select onChange={(e) => handleSortBy(e.target.value)}>
-              <option value="">-- Select --</option>
-              <option value="year">Year</option>
-            </select>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>{error}</h1>}
+
+      <div className="sort-bar">
+        <label>Sort by:</label>
+        <select onChange={(e) => handleSortBy(e.target.value)}>
+          <option value="">-- Select --</option>
+          <option value="year">Year</option>
+        </select>
       </div>
 
-         <div className="movie-container">
+      <div className="movie-container">
         {data.map((el, index) => (
           <div key={index} className="card-items">
-            
             <img src={el.Poster} alt={el.Title} />
-                 
 
-               <div className="add-view-btn">
-               <Link to={`movie/${el.imdbID}`}><button className="view"> View</button>  </Link> 
+            <div className="add-view-btn">
+              <Link to={`movie/${el.imdbID}`}>
+                <button className="view"> View</button>
+              </Link>
+              <button className="add" onClick={() => handleAdd(el)}>
+                Fav
+              </button>
+            </div>
 
-                  <button className="add" onClick={()=>handleAdd(el)} >Fav</button>
-
-               </div>
-                
-               <b className="year">Year : {el.Year}</b>
-        
+            <b className="year">Year : {el.Year}</b>
           </div>
         ))}
+        {!loading && data.length === 0 && <h1>No results found.</h1>}
       </div>
-    
-         
-   
-
-
-      
     </>
   );
 };
 
 export default Home;
-
-
